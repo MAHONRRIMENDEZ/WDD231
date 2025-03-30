c00a84cf04630dab99dd8aaec50da3a6abc4b971
MAHONRRIMENDEZ <97697808+MAHONRRIMENDEZ@users.noreply.github.com>
2025-03-29 20:46:35 -0600

    completed

---
+++ chamber/scripts/home.js
@@ -0,0 +1,47 @@
+// Current year
+function getCurrentYear() {
+    const currentYearElement = document.getElementById('currentyear');
+    const currentYear = new Date().getFullYear();
+    currentYearElement.textContent = currentYear;
+}
+
+function getLastModifiedDate() {
+    const lastModifiedElement = document.getElementById('lastModified');
+    const lastModified = new Date(document.lastModified);
+    const formattedDate = `${lastModified.getDate()}/${lastModified.getMonth() + 1}/${lastModified.getFullYear()} ${lastModified.getHours()}:${lastModified.getMinutes().toString().padStart(2, '0')}`;
+    lastModifiedElement.textContent = `Last Modified: ${formattedDate}`;
+}
+getCurrentYear();
+getLastModifiedDate();
+
+// Hambuerguer menu
+
+const hamburguerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('.menuLinks');
+
+hamburguerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburguerElement.classList.toggle('open')
+})
+
+// number of visits
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} 
+else {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
+// Get current date and time
+var currentTimestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
+document.getElementById("timestamp").value = currentTimestamp;
